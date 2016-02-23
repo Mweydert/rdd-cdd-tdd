cf16c93ca9623bd1de9ce36b479fb36176b8c057
Weydert Michael <michael.weydert@devinci.fr>
2016-02-23 15:42:36 +0100

    2 test of config-test.js

--- weather/test/test-config.js
+++ weather/test/test-config.js
@@ -1,11 +1,25 @@
 var assert = require('assert'),
     config = require('./../lib/config');
 
+var units = { type: "string", temp: 12 , wind:12};
+var ip = '1212';
+
+//create a variable config with the 2 variables previously declared
+var Config = config(units, ip);
 
 describe('Config', function () {
-    it('has 3 props')
 
-    it('after construction, 2 props are populated')
+    it('has 3 props', function(){
+      assert(Object.keys(Config).length == 3); //check the length of our variable
+    });
 
-    it('has correct key types')
+    it('after construction, 2 props are populated', function(){
+
+    });
+
+    it('has correct key types', function(){
+       assert(typeof(Config.ip) == "string");
+       assert(typeof(Config.units) == "object");
+    });
+
 });
